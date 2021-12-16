ed6206977aa8caca6cf72f242e53fa9072505a1b
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-16 13:39:07 +0000

    Create lesson1.1.py

---
+++ Lesson01/exercises/lesson1.1.py
@@ -0,0 +1,13 @@
+#Object types
+#AngryAdmin -> https://https://angrysysops.com
+#In this lesson we will be work with numeric and string assigment, and learn how to check variable type 
+#int, float, str, type()
+
+#TASK:
+#Create three variable and assign 5 as integer, float and string. Print each of thPrint, and use the type()
+# function to print the value and type of one or more of them.
+
+
+
+
+
