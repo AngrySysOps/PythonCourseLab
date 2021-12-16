29b8017e013c00ee885c4ecba2c881689409b880
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-16 14:45:51 +0000

    Create lesson1.5.py

---
+++ Lesson01/exercises/lesson1.5.py
@@ -0,0 +1,11 @@
+#Object types
+#AngryAdmin -> https://https://angrysysops.com
+
+#Concatenation operator
+#TASK:
+#Use '+' to concatenate a, b, c and dtogether.
+
+a = 'Angry'
+b = 'Sys'
+c = 'Ops'
+d = '.com'
