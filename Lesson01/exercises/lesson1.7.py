50d45b9c5f0a679c064c5502ff0896b2f30e7932
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-16 17:09:40 +0000

    Create lesson1.7.py

---
+++ Lesson01/exercises/lesson1.7.py
@@ -0,0 +1,9 @@
+#AngryAdmin -> https://https://angrysysops.com
+#Get the length of strings with len()
+
+# Print the length of each of the below strings. 
+# Both strings should be length of 11.
+
+x = 'hello there'
+
+y = 'hi       ok'
