3865837e4f3ac701f938200ccabafb5d86fec0bd
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-16 14:47:53 +0000

    Create lesson1.6.py

---
+++ Lesson01/exercises/lesson1.6.py
@@ -0,0 +1,7 @@
+#Object types
+#AngryAdmin -> https://https://angrysysops.com
+
+#String repetition operator
+#Use '*' to repeat the string in hello 3 times to return a new str value yoyoyo.
+
+hello = 'yo'
