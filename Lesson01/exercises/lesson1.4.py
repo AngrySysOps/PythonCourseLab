5bd4cadb62dd7266f9079167823642da46b8939d
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-16 14:42:28 +0000

    Create lesson1.4.py

---
+++ Lesson01/exercises/lesson1.4.py
@@ -0,0 +1,9 @@
+#Object types
+#AngryAdmin -> https://https://angrysysops.com
+
+#Modulus operator
+# What is a % b?  What is a % c?  Can you tell why this operator is returning these values?
+
+a = 8
+b = 5
+c = 2
