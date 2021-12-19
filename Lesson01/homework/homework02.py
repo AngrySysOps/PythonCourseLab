aa7aa58199e3bcc43c1b76632cce15c429528040
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-19 21:34:35 +0000

    Create homework02.py

---
+++ Lesson01/homework/homework02.py
@@ -0,0 +1,9 @@
+# TASK 2:
+# Multiply 2 number, then  square the results - everything in one line
+# Print output
+
+a = 8
+b = 12
+
+
+# Hint: Expected output 9216, also remeber about operators precedence.
