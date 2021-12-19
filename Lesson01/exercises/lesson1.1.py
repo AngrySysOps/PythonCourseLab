28dc756f3ddf20821a91b91d848300cdb221aafc
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2021-12-19 22:04:32 +0000

    Create lesson1.1.py

---
+++ Lesson01/exercises/lesson1.1.py
@@ -0,0 +1,10 @@
+# AngryAdmin -> https://https://angrysysops.com
+# Operators Precedence
+# More about Operator Precednce on my blog :
+# https://angrysysops.com/2021/12/19/python-operators-precedence/
+
+a = 2
+b = 4
+c = 6
+d = 2
+
