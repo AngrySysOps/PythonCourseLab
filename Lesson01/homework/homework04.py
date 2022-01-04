23b24a531edbf6f17b1be4176c6bd75c65f92b3c
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2022-01-04 15:41:28 +0000

    Create homework04.py

---
+++ Lesson01/homework/homework04.py
@@ -0,0 +1,12 @@
+# TASK 4:
+# You went to restaurant with your friends.
+# Your bill 150 euro where there are 5 people in your party. 
+# You need to calculate 20 % tip.
+# How much each person must pay? 
+
+bill = 150
+people = 5
+tip_prc = 20
+
+
+# HINT: Expected output = 26.0 (float)
